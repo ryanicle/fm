70350894eefac900f9ba1a4d6aa052c76750207b
Ryan Ye <ryanicle@gmail.com>
2018-07-15 18:23:31 +0800

    Add test cases for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Create user" do
+    user = User.new(email: 'test3@example.com', password: 'User#Test')
+    assert user.save
+  end
+
+  test "Should not create user if email already exists" do
+    user = User.new(email: 'test1@example.com', password: 'User#Test')
+    assert_not user.save
+  end
 end
